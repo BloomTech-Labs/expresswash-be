d445a992fb6ec369a29663831d46a570fc8449f2
cahaug <caleahaug@gmail.com>
2019-12-18 13:33:21 -0700

    postgres database working for authentication with bcrypt

--- database/queries.js
+++ database/queries.js
@@ -3,8 +3,15 @@ const knex = require('./knex');
 module.exports = { 
     getAllUsers(){
         return knex('users');
-    }
-    
+    },
+
+    insertUserPG(user){
+        return knex('users').insert(user);
+    },
+
+    singleUserForLogin(email){
+        return knex('users').where("email", email);
+    },
 
 
 
