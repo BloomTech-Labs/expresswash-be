ebc58474f62e7b951ef1bc524d5879a8c9343c00
chris adams <cladams0203@gmail.com>
2020-04-21 21:55:38 -0400

    added environments to the knexfile for prod, dev, test (#16)

--- knexfile.js
+++ knexfile.js
@@ -1,67 +1,94 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-    development: {
-      client: 'pg',
-      // useNullAsDefault: true,
-      connection: 'postgres://localhost/databasePG',
-      // connection: process.env.DATABASE_URL,
-      // pool: {
-      //   afterCreate: (conn, done) => {
-      //     conn.run('PRAGMA foreign_keys = ON', done);
-      //   },
-      // },
-      // useNullAsDefault: true,
-      migrations: {
-        directory: './database/migrationsPG',
-      },
-      seeds: {
-        directory: './database/seedsPG',
-      },
+  development: {
+    client: "pg",
+    connection: {
+      host: process.env.LOCAL_DB_HOST,
+      port: process.env.LOCAL_DB_PORT,
+      user: process.env.LOCAL_DB_USER,
+      password: process.env.LOCAL_DB_PASSWORD,
+      database: process.env.LOCAL_DB_DATABASE_DEV,
     },
-    testing: {
-      client: 'sqlite3',
-      useNullAsDefault: true,
-      connection: {
-        filename: './database/test.db3',
-      },
-      // connection: process.env.DATABASE_URL,
-      migrations: {
-        directory: './database/migrations',
-      },
-      seeds: {
-        directory: './database/seeds',
-      },
+    pool: {
+      min: 2,
+      max: 10,
     },
-    production: {
-      client: 'pg',
-      // useNullAsDefault: true,
-      connection: process.env.DATABASE_URL,
-        
-        // user: `${process.env.DB_USER}`, 
-        // password: `${process.env.DB_PW}`,
-        // host:`${process.env.DB_HOST}`,
-        // port:`${process.env.DB_PORT}`
-      // connection: process.env.DB_URL,
-      migrations: {
-        directory: './database/migrationsPG',
-      },
-      seeds: { 
-        directory: './database/seedsPG' 
-      },
-      // production: {
-      //   client: 'sqlite3',
-      //   useNullAsDefault: true,
-      //   connection: {
-      //     filename: './database/database.db3',
-      //   },
-      //   // connection: process.env.DB_URL,
-      //   migrations: {
-      //     directory: './database/migrations',
-      //   },
-      //   seeds: { 
-      //     directory: './database/seeds' 
-      //   },
+    // useNullAsDefault: true,
+    // connection: 'postgres://localhost/databasePG',
+    // connection: process.env.DATABASE_URL,
+    // pool: {
+    //   afterCreate: (conn, done) => {
+    //     conn.run('PRAGMA foreign_keys = ON', done);
+    //   },
+    // },
+    // useNullAsDefault: true,
+    migrations: {
+      directory: "./database/migrationsPG",
     },
-  };
-  
+    seeds: {
+      directory: "./database/seedsPG",
+    },
+  },
+  testing: {
+    client: "pg",
+    connection: {
+      host: process.env.LOCAL_DB_HOST,
+      port: process.env.LOCAL_DB_PORT,
+      user: process.env.LOCAL_DB_USER,
+      password: process.env.LOCAL_DB_PASSWORD,
+      database: process.env.LOCAL_DB_DATABASE_TEST,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+  },
+  production: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: "postgres",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    // useNullAsDefault: true,
+    // connection: process.env.DATABASE_URL,
+
+    // user: `${process.env.DB_USER}`,
+    // password: `${process.env.DB_PW}`,
+    // host:`${process.env.DB_HOST}`,
+    // port:`${process.env.DB_PORT}`
+    // connection: process.env.DB_URL,
+    migrations: {
+      directory: "./database/migrationsPG",
+    },
+    seeds: {
+      directory: "./database/seedsPG",
+    },
+    // production: {
+    //   client: 'sqlite3',
+    //   useNullAsDefault: true,
+    //   connection: {
+    //     filename: './database/database.db3',
+    //   },
+    //   // connection: process.env.DB_URL,
+    //   migrations: {
+    //     directory: './database/migrations',
+    //   },
+    //   seeds: {
+    //     directory: './database/seeds'
+    //   },
+  },
+};
