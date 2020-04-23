fd3c9900b987d927860e8b7e99dca089917bedca
chris adams <cladams0203@gmail.com>
2020-04-22 22:49:31 -0400

    Feature/new migrations (#17)
    * refactored migrations to accomodate mobile app
    
    * fixed typo in cars migration
    
    * added rating to the washer table

--- database/migrationsPG/20191217191729_create-users-table-pg.js
+++ database/migrationsPG/20191217191729_create-users-table-pg.js
@@ -1,153 +1,98 @@
-
-// exports.up = function(knex, Promise) {
-//   return knex.schema.createTable('users', (table) => {
-//       table.increments('id');
-//       table.text('accountType');
-//       table.text('email');
-//       table.text('password');
-//       table.text('firstName');
-//       table.text('lastName');
-//       table.text('phoneNumber');
-//       table.text('stripeUUID');
-//       table.text('streetAddress');
-//       table.text('streetAddress2');
-//       table.text('city');
-//       table.text('state');
-//       table.text('zip');
-//       table.text('workStatus');
-//       table.text('profilePicture');
-//       table.text('creationDate');
-//   })
-// };
-
-// exports.down = function(knex, Promise) {
-//   return knex.schema.dropTable('users');
-// };
-
-
-exports.up = function(knex, Promise) {
-    return knex.schema
-    .createTable('users', user => {
-        user.increments('id');
-        user.string('accountType',128).notNullable();
-        user.string('email', 128)
-            .notNullable()
-            .unique();
-        user.string('password', 128).notNullable();
-        user.string('firstName', 128).notNullable();
-        user.string('lastName', 128).notNullable();
-        user.string('phoneNumber', 25).notNullable();
-        user.string('stripeUUID', 128).unique();
-        user.string('streetAddress', 400).notNullable();
-        user.string('streetAddress2', 400);
-        user.string('city', 400).notNullable();
-        user.string('state', 400).notNullable();
-        user.string('zip', 400).notNullable();
-        user.boolean('workStatus').notNullable().defaultTo(false);
-        user.string('profilePicture', 400).defaultTo('');
-        user.string('creationDate', 128).notNullable();
+exports.up = function (knex, Promise) {
+  return knex.schema
+    .createTable("users", (user) => {
+      user.increments("id");
+      user.string("accountType", 128).notNullable();
+      user.string("email", 128).notNullable().unique();
+      user.string("password", 128).notNullable();
+      user.string("firstName", 128).notNullable();
+      user.string("lastName", 128).notNullable();
+      user.string("phoneNumber", 25);
+      user.string("stripeUUID", 128).unique();
+      user.string("streetAddress", 400);
+      user.string("streetAddress2", 400);
+      user.string("city", 400);
+      user.string("state", 400);
+      user.string("zip", 400);
+      user.boolean("workStatus").defaultTo(false);
+      user.string("profilePicture", 400).defaultTo("");
+      user.string("bannerImage", 400).defaultTo("");
+      user.string("creationDate", 128);
+      user.integer("userRating");
     })
-    .createTable('cars', cars => {
-        cars.increments('carId');
-        cars.string('make', 128).notNullable();
-        cars.string('model', 128).notNullable();
-        cars.string('category', 128).notNullable();
-        cars.string('size', 128).notNullable();
+    .createTable("cars", (cars) => {
+      cars.increments("carId");
+      cars
+        .integer("clientId")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      cars.string("make", 128).notNullable();
+      cars.string("model", 128).notNullable();
+      cars.integer("year").notNullable();
+      cars.string("color", 60).notNullable();
+      cars.string("licensePlate", 10).notNullable();
+      cars.string("photo", 400);
+      cars.string("category", 128).notNullable();
+      cars.string("size", 128).notNullable();
     })
-    .createTable('clientCars', clientCars => {
-        clientCars.increments('clientCarId');
-        clientCars.integer('carId')
-            .unsigned()
-            .notNullable()
-            .references('carId')
-            .inTable('cars')
-            .onDelete('RESTRICT')
-            .onUpdate('RESTRICT');
-        clientCars.integer('id')
-            .unsigned()
-            .notNullable()
-            .references('id')
-            .inTable('users')
-            .onDelete('RESTRICT')
-            .onUpdate('RESTRICT');
-        clientCars.string('color').notNullable();
-        clientCars.string('licensePlate', 9).notNullable();
+    .createTable("jobs", (jobs) => {
+      jobs.increments("jobId");
+      jobs.string("washAddress", 128).notNullable();
+      jobs.boolean("scheduled").notNullable().defaultTo(true);
+      jobs.boolean("completed").notNullable().defaultTo(false);
+      jobs.boolean("paid").notNullable().defaultTo(false);
+      jobs
+        .integer("clientId")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      jobs.integer("washerId");
+      jobs.string("creationDate", 128).notNullable();
+      jobs.integer("carId").notNullable();
+      jobs.string("address").notNullable();
+      jobs.string("address2");
+      jobs.string("jobLocation");
+      jobs.string("city").notNullable();
+      jobs.string("state").notNullable();
+      jobs.string("zip").notNullable();
+      jobs.string("notes");
+      jobs.string("jobType").notNullable();
+      jobs.string("photoBeforeJob");
+      jobs.string("photoAfterJob");
+      jobs.string("timeRequested").notNullable();
+      jobs.string("timeCompleted");
     })
-    .createTable('jobs', jobs => {
-        jobs.increments('jobId');
-        jobs.string('washAddress', 128).notNullable();
-        jobs.boolean('scheduled').notNullable().defaultTo(true);
-        jobs.boolean('completed').notNullable().defaultTo(false);
-        jobs.boolean('paid').notNullable().defaultTo(false);
-        jobs.integer('clientId')
-            .unsigned()
-            .notNullable()
-            .references('id')
-            .inTable('users')
-            .onDelete('RESTRICT')
-            .onUpdate('RESTRICT');
-        jobs.integer('clientCarId')
-            .unsigned()
-            .references('clientCarId')
-            .inTable('clientCars')
-            .onDelete('RESTRICT')
-            .onUpdate('RESTRICT');
-        jobs.integer('washerId')
-            .unsigned()
-            .references('id')
-            .inTable('users')
-            .onDelete('RESTRICT')
-            .onUpdate('RESTRICT');
-        jobs.string('creationDate', 128).notNullable();
-
-    })
-    .createTable('ratingsOW', ratingsOW => {
-        ratingsOW.increments('ratingOWId');
-        ratingsOW.integer('rating', 2).notNullable();
-        ratingsOW.string('notes', 400);
-        ratingsOW.integer('id')
-            .unsigned()
-            .notNullable()
-            .references('id')
-            .inTable('users')
-            .onDelete('RESTRICT')
-            .onUpdate('RESTRICT');
-        // ratingsOW.integer('clientId')
-        //     .unsigned()
-        //     .notNullable()
-        //     .references('id')
-        //     .inTable('users')
-        //     .onDelete('RESTRICT')
-        //     .onUpdate('RESTRICT');
-    })
-    .createTable('ratingsOC', ratingsOC => {
-        ratingsOC.increments('ratingOCId');
-        ratingsOC.integer('rating', 2).notNullable();
-        ratingsOC.string('notes', 400);
-        ratingsOC.integer('id')
-            .unsigned()
-            .notNullable()
-            .references('id')
-            .inTable('users')
-            .onDelete('RESTRICT')
-            .onUpdate('RESTRICT');
-        // ratingsOC.integer('washerId')
-        //     .unsigned()
-        //     .notNullable()
-        //     .references('id')
-        //     .inTable('users')
-        //     .onDelete('RESTRICT')
-        //     .onUpdate('RESTRICT');
+    .createTable("washers", (washer) => {
+      washer.increments("washerId");
+      washer
+        .integer("userId")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      washer.integer("rateSmall");
+      washer.integer("rateMedium").notNullable();
+      washer.integer("rateLarge");
+      washer.string("aboutMe", 200);
+      washer.string("currentLocation");
+      washer.boolean("available").defaultTo(false);
+      washer.integer("washerRating");
     });
 };
 
-exports.down = function(knex, Promise) {
-    return knex.schema
-        .dropTableIfExists('ratingsOC')
-        .dropTableIfExists('ratingsOW')
-        .dropTableIfExists('jobs')
-        .dropTableIfExists('clientCars')
-        .dropTableIfExists('cars')
-        .dropTableIfExists('users');
+exports.down = function (knex, Promise) {
+  return knex.schema
+    .dropTableIfExists("washers")
+    .dropTableIfExists("jobs")
+    .dropTableIfExists("cars")
+    .dropTableIfExists("users");
 };
-
