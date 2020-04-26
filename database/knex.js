f4eff6e20fe3224b895081c9df17eec8af7fa297
cladams0203 <cladams0203@gmail.com>
2020-04-25 23:02:25 -0400

    added tests for code coverage to server test file

--- database/knex.js
+++ database/knex.js
@@ -1,7 +1,7 @@
-const environment = process.env.NODE_ENV || 'development';
-const config = require('../knexfile');
-const environmentConfig = config[environment];
-const knex = require('knex');
-const connection = knex(environmentConfig);
+// const environment = process.env.NODE_ENV || 'development';
+// const config = require('../knexfile');
+// const environmentConfig = config[environment];
+// const knex = require('knex');
+// const connection = knex(environmentConfig);
 
-module.exports = connection;
+// module.exports = connection;
