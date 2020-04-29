619078a345ed3b5c22bdeab574f89b59bea9af93
DentonWarnock <dentonwarnock@gmail.com>
2020-04-28 19:07:08 -0700

    unit tests added for all user-router endpoints and possible responses.

--- users/users-router.test.js
+++ users/users-router.test.js
@@ -23,3 +23,75 @@ test("/users/ GET request - return error and status 500 on failure", async () =>
   expect(res.status).toBe(500);
   mock.mockRestore();
 });
+
+test("/users/:id GET request - return user by id with response status 200", async () => {
+  const mock = jest.spyOn(Users, "findById");
+  mock.mockImplementationOnce(() => Promise.resolve(2));
+  const res = await request(server).get("/2");
+  expect(res.status).toBe(200);
+  mock.mockRestore();
+});
+
+test("/users/:id GET request - return response status 404 when no user", async () => {
+  const mock = jest.spyOn(Users, "findById");
+  mock.mockImplementationOnce(() => Promise.resolve());
+  const res = await request(server).get("/1");
+  expect(res.status).toBe(404);
+  mock.mockRestore();
+});
+
+test("/users/:id DELETE request - return response status 200 when deleting a valid user", async () => {
+  const mock = jest.spyOn(Users, "findById");
+  mock.mockImplementationOnce(() => Promise.resolve(1));
+  const res = await request(server).delete("/1");
+  expect(res.status).toBe(200);
+  mock.mockRestore();
+});
+
+test("/users/:id DELETE request - return response status 404 when no user", async () => {
+  const mock = jest.spyOn(Users, "del");
+  mock.mockImplementationOnce(() => Promise.resolve());
+  const res = await request(server).delete("/2");
+  expect(res.status).toBe(404);
+  mock.mockRestore();
+});
+
+test("/users/:id DELETE request - return response status 500 when server error occurs", async () => {
+  const mock = jest.spyOn(Users, "findById");
+  mock.mockImplementationOnce(() => Promise.reject(1));
+  const res = await request(server).delete("/1");
+  expect(res.status).toBe(500);
+  mock.mockRestore();
+});
+
+changes = { firstName: "new", lastName: "old" };
+
+test("/users/:id PUT request - return response status 200 when user successfully updated", async () => {
+  const mock = jest.spyOn(Users, "findById");
+  const mockUpdate = jest.spyOn(Users, "update");
+  mock.mockImplementationOnce(() => Promise.resolve(1));
+  mockUpdate.mockImplementationOnce(() => Promise.resolve(changes));
+  const res = await request(server).put("/1");
+  expect(res.status).toBe(200);
+  mock.mockRestore();
+});
+
+test("/users/:id PUT request - return response status 404 no user to be updated", async () => {
+  const mock = jest.spyOn(Users, "findById");
+  const mockUpdate = jest.spyOn(Users, "update");
+  mock.mockImplementationOnce(() => Promise.resolve());
+  mockUpdate.mockImplementationOnce(() => Promise.resolve(changes));
+  const res = await request(server).put("/1");
+  expect(res.status).toBe(404);
+  mock.mockRestore();
+});
+
+test("/users/:id PUT request - return response status 500 when server error occurs", async () => {
+  const mock = jest.spyOn(Users, "findById");
+  const mockUpdate = jest.spyOn(Users, "update");
+  mock.mockImplementationOnce(() => Promise.resolve());
+  mockUpdate.mockImplementationOnce(() => Promise.resolve(changes));
+  const res = await request(server).put("/1");
+  expect(res.status).toBe(404);
+  mock.mockRestore();
+});
