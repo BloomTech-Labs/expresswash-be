920188c3572402d23bfc426565a8b61f068d4042
Tommy Kindle <51128524+tommykindle@users.noreply.github.com>
2020-04-28 20:18:42 -0700

    Feature/jobs router model (#28)
    * added notes for potential changes in comments
    
    * finished reasoning about the jobs endpoint decision reached to refactor everything
    
    * removed helper functions from queries file built crud helpers in jobs-model.js
    
    * added notes for potential changes in comments
    
    * finished reasoning about the jobs endpoint decision reached to refactor everything
    
    * removed helper functions from queries file built crud helpers in jobs-model.js
    
    * took out unneeded routes in jobs router and created new ones.fix old nxt
    
    * working on jobs router
    
    * modified routers in jobsrouter, 80% complete
    
    * pushing merged changes
    
    * fixed addWasher to job route
    
    * added jobs-router tests 4 currently passing
    
    * added test for getting job infor and adding washer to job
    
    * added test for deleting jobs by id
    
    * finished tests for jobs endpoints
    
    Co-authored-by: Ethret <Ethret@users.noreply.github.com>

--- database/queries.js
+++ database/queries.js
@@ -1,96 +1,73 @@
-const knex = require('./knex');
+const knex = require("./dbConfig.js");
 
-module.exports = { 
-    getAllUsers(){
-        return knex('users');
-    },
+module.exports = {
+  getAllUsers() {
+    return knex("users");
+  },
 
-    getNewUser(){
-        return knex('users')
-    },
+  getNewUser() {
+    return knex("users");
+  },
 
-    insertUserPG(user){
-        return knex('users').insert(user);
-    },
+  insertUserPG(user) {
+    return knex("users").insert(user);
+  },
 
-    singleUserForLogin(email){
-        return knex('users').where("email", email);
-    },
+  singleUserForLogin(email) {
+    return knex("users").where("email", email);
+  },
 
-    getCarMakes(){
-        return knex('cars').select('make').count().groupBy('make').orderBy('make', 'asc');
-    },
+  getCarMakes() {
+    return knex("cars")
+      .select("make")
+      .count()
+      .groupBy("make")
+      .orderBy("make", "asc");
+  },
 
-    getCarModelsForMake(make){
-        return knex('cars').where({make}).select('carId', 'model',);
-    },
+  getCarModelsForMake(make) {
+    return knex("cars").where({ make }).select("carId", "model");
+  },
 
-    myCars(id){ 
-        return knex('clientCars').where({ id }).select('carId', 'color', 'licensePlate',);
-    },
+  myCars(id) {
+    return knex("clientCars")
+      .where({ id })
+      .select("carId", "color", "licensePlate");
+  },
 
-    addACar(clientCar){
-        return knex('clientCars').insert(clientCar)
-    },
+  addACar(clientCar) {
+    return knex("clientCars").insert(clientCar);
+  },
 
-    getLatestReviewOfWasher(id){
-        return knex('ratingsOW').first().where({ id })
-    },
+  getLatestReviewOfWasher(id) {
+    return knex("ratingsOW").first().where({ id });
+  },
 
-    getLatestReviewOfClient(id){
-        return knex('ratingsOC').first().where({ id })
-    },
+  getLatestReviewOfClient(id) {
+    return knex("ratingsOC").first().where({ id });
+  },
 
-    getLatestJobClient(clientId){
-        return knex('jobs').where({ clientId }).orderBy('creationDate', 'desc').first()
-    },
+  getWasherInfo(id) {
+    return knex("users").where({ id });
+  },
 
-    addNewJob(newJob){
-        return knex('jobs').insert(newJob)
-    },
+  getWorkStatus(id) {
+    return knex("users").where({ id }).select("id", "workStatus");
+  },
 
-    seeAvailableJobs(){
-        return knex('jobs').where( "washerId", null )
-    },
+  setWorkStatus(id, workStatus) {
+    return knex("users").where({ id }).update({ workStatus });
+  },
 
-    getWasherInfo(id){
-        return knex('users').where({ id })
-    },
+  getWasherRatings(id) {
+    return knex("ratingsOW").where({ id }).select("rating");
+  },
 
-    selectJobById(jobId, washerId){
-        return knex('jobs').where({ jobId })
-    },
+  getClientRatings(id) {
+    return knex("ratingsOC").where({ id }).select("rating");
+  },
 
-    getWorkStatus(id){
-        return knex('users').where({ id }).select('id', 'workStatus')
-    },
-
-    setWorkStatus(id, workStatus){
-        return knex('users').where({ id }).update({ workStatus })
-    },
-
-    setWasherOnJob(jobId, washerId){
-        return knex('jobs').where({ jobId }).update({ washerId })
-    },
-
-    getWasherRatings(id){
-        return knex('ratingsOW').where({ id }).select('rating')
-    },
-
-    getClientRatings(id){
-        return knex('ratingsOC').where({ id }).select('rating')
-    },
-
-    rateWasher(washerRating){
-        return knex('ratingsOW').insert(washerRating)
-    },
-
-    rateClient(clientRating){
-        return knex('ratingsOC').insert(clientRating)
-    },
-
-    countWasherOnJobs(washerId){
-        return knex('jobs').where({ washerId }).count()
-    }
-
-}
+  rateWasher(washerRating) {
+    return knex("ratingsOW").insert(washerRating);
+  },
+};
