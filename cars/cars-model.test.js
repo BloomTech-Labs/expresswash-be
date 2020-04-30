0d1a93fba7dc01bdc67c935dccd38900c11fb97a
Jeffrey <jeffreymartespolanco@hotmail.com>
2020-04-30 19:56:14 -0400

    Finished testing cars-model with all test passing

--- cars/cars-model.test.js
+++ cars/cars-model.test.js
@@ -18,28 +18,49 @@ const expectedObject = {
 
 test("testing cars model getAll", async () => {
   const res = await model.getAll();
+  let resObject = {};
+  for (let keys of res) {
+    resObject = keys;
+  }
 
-  expect(res).toMatchObject(expectedObject);
+  expect(resObject).toMatchObject(expectedObject);
 });
 
-// test("testing cars model findBy", async () =>{
-//      const
-// })
+test("testing cars model findBy", async () => {
+  const res = await model.findBy(1);
+  expect(res).toHaveProperty("make");
+});
+
+test("testing card model addCar", async () => {
+  const res = await model.addCar({});
+  expect(res).toHaveProperty("year", 2005);
+});
+
+test("testing cars model update", async () => {
+  const res = await model.update({ color: "Golden" }, 1);
+  expect(res).toHaveLength(1);
+});
+test("testing cars model delete", async () => {
+  const res = await model.remove(1);
+  expect(res).toHaveLength(1);
+});
 
 // tracker for incoming sql queries and sets response
 const tracker = require("mock-knex").getTracker();
 tracker.install();
 tracker.on("query", (query) => {
-  query.response({
-    carId: 1,
-    clientId: 1,
-    make: "gefdf",
-    model: "morneo",
-    year: 2005,
-    color: "Gold",
-    licensePlate: "Nissanfeva",
-    photo: "",
-    category: "SUV",
-    size: "M",
-  });
+  query.response([
+    {
+      carId: 1,
+      clientId: 1,
+      make: "gefdf",
+      model: "morneo",
+      year: 2005,
+      color: "Gold",
+      licensePlate: "Nissanfeva",
+      photo: "",
+      category: "SUV",
+      size: "M",
+    },
+  ]);
 });
