744ec8352f0196e062fd18f89e7ee0e41d444cee
cladams0203 <cladams0203@gmail.com>
2020-05-01 22:57:57 -0400

    fixed bug on test deployment

--- users/users-router.js
+++ users/users-router.js
@@ -51,13 +51,7 @@ usersRouter.put("/rating/:id", (req, res) => {
             res.status(201).json(user);
           })
           .catch((err) => {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
             console.log(err);
->>>>>>> fixed tests in users router
-=======
->>>>>>> checked all endpoints locally and pass
             res.status(500).json({
               message: "error updating the user rating",
             });
