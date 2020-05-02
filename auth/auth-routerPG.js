d28671998b79523b281833792da2b5015df7f957
cladams0203 <cladams0203@gmail.com>
2020-05-02 00:37:55 -0400

    still chasing waterfalls

--- auth/auth-routerPG.js
+++ auth/auth-routerPG.js
@@ -14,28 +14,27 @@ authRouterPG.get("/", (req, res) => {
     });
 });
 
-authRouterPG.post("/registerClient", async (req, res) => {
+authRouterPG.post("/registerClient", (req, res) => {
   let user = req.body;
   const date = new Date();
   const creationDate = date;
   const hash = bcrypt.hashSync(user.password, 10);
   user.password = hash;
   user = { ...user, creationDate };
-  try {
-    // console.log(user);
-    const user2 = await Users.insert(user);
-    const newUser = user2;
-    delete newUser.password;
-    const token = generateToken(newUser);
-    console.log(newUser);
-    res.status(201).json({
-      message: "user created successfully",
-      token,
-      user: newUser,
+  return Users.insert(user)
+    .then((user2) => {
+      const newUser = user2;
+      delete newUser.password;
+      const token = generateToken(newUser);
+      res.status(201).json({
+        message: "user created successfully",
+        token,
+        user: newUser,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "unable to register new user" });
     });
-  } catch (err) {
-    res.status(500).json({ message: "unable to register new user" });
-  }
 });
 
 authRouterPG.post(
