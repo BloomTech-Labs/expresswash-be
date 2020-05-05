65bb9c85bead1bccc8407c05405258accba14962
chris adams <cladams0203@gmail.com>
2020-05-04 23:26:13 -0400

    QA testing/docs update (#44)
    * merging
    
    * fixing
    
    * finished detailed docs for the auth routes
    
    * updated users endpoint docs
    
    * finished cars endpoint documentation
    
    * updated jobs documentation
    
    * added some headings and current env requirements
    
    * updated package json for testing
    
    * cleaned up server test
    
    * finished detailed docs for the auth routes
    
    * updated users endpoint docs
    
    * finished cars endpoint documentation
    
    * updated jobs documentation
    
    * added some headings and current env requirements
    
    * updated package json for testing
    
    * cleaned up server test
    
    * edited json file for tests
    
    * finished all docs up to date
    
    * merging

--- api/server.test.js
+++ api/server.test.js
@@ -14,37 +14,7 @@ test("server error for 404", async () => {
   const res = await request(testServer).get("/a");
   expect(res.status).toBe(404);
 });
-
-// describe("the server is running", () => {
-//   const credentials = {
-//     email: "test@test.con",
-//     firstName: "Vegan",
-//     lastName: "Falafel",
-//     password: "1234",
-//     phoneNumber: "1234567890",
-//     streetAddress: "1051 Market St",
-//     streetAddress2: "APT 240",
-//     city: "San Francisco",
-//     State: "California",
-//     zip: "94103",
-//   };
-
-//   describe("the server is running message", () => {
-//     it("should return a specific message", async () => {
-//       // test setup
-//       return request(server)
-//         .get("/")
-//         .then((res) => {
-//           expect(res.body.message).toBe("Backend is up and running");
-//         });
-//     });
-//     it("should return a status 200", async () => {
-//       //test setup
-//       return request(server)
-//         .get("/")
-//         .then((res) => {
-//           expect(res.status).toBe(200);
-//         });
-//     });
-//   });
-// });
+test("server error for 404", async () => {
+  const res = await request(testServer).get("/a");
+  expect(res.status).toBe(404);
+});
