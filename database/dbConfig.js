6af09abdee04c75f84d4cddc747c22373627c582
cladams0203 <cladams0203@gmail.com>
2020-05-09 21:39:41 -0400

    /

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,8 +1,9 @@
-const knex = require('knex');
-const config = require('../knexfile.js');
+const knex = require("knex");
+const config = require("../knexfile.js");
 
-const environment = process.env.DB_ENV || 'development';
+const environment = process.env.DB_ENV || "development";
 
 // console.log('environment:', environment);
 
 module.exports = knex(config[environment]);
+//I was here
