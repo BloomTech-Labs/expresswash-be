c786e90be385b61874ae3ad4d775253ae3a47dbb
chris adams <cladams0203@gmail.com>
2020-05-21 21:50:56 -0400

    fixed all be tests for updated code (#63)

--- cars/cars-routerPG.test.js
+++ cars/cars-routerPG.test.js
@@ -96,6 +96,8 @@ test("/cars/ POST request - returns missing car body error - status code 400", a
 test("/cars/:carId PUT request - returns the modified car - status code 200", async () => {
   const mock = jest.spyOn(Cars, "update");
   const mockUserFindById = jest.spyOn(Users, "findById");
+  const mockCarFind = jest.spyOn(Cars, "findBy");
+  mockCarFind.mockImplementationOnce(() => Promise.resolve(newCar));
   mockUserFindById.mockImplementationOnce(() => Promise.resolve(1));
   mock.mockImplementationOnce(() => Promise.resolve());
   const res = await request(server).put("/6").send(newCar);
@@ -103,6 +105,7 @@ test("/cars/:carId PUT request - returns the modified car - status code 200", as
   expect(res.body).toMatchObject({ message: res.body.message });
   mock.mockRestore();
   mockUserFindById.mockRestore();
+  mockCarFind.mockRestore();
 });
 
 test("/cars/:carId PUT request - returns car does not exist error - status code 400", async () => {
@@ -119,6 +122,8 @@ test("/cars/:carId PUT request - returns car does not exist error - status code 
 
 test("/cars/:carId PUT request - returns that user does not exist error - status code 400", async () => {
   const mock = jest.spyOn(Cars, "update");
+  const mockCarFind = jest.spyOn(Cars, "findBy");
+  mockCarFind.mockImplementationOnce(() => Promise.resolve(newCar));
   const mockUserFindById = jest.spyOn(Users, "findById");
   mockUserFindById.mockImplementationOnce(() => Promise.resolve(false));
   mock.mockImplementationOnce(() => Promise.resolve());
@@ -127,26 +132,32 @@ test("/cars/:carId PUT request - returns that user does not exist error - status
   expect(res.body).toMatchObject({ error: res.body.error });
   mock.mockRestore();
   mockUserFindById.mockRestore();
+  mockCarFind.mockRestore();
 });
 
 test("/cars/:carId PUT request - returns error- status code 500", async () => {
   const mock = jest.spyOn(Cars, "update");
+  const mockCarFind = jest.spyOn(Cars, "findBy");
+  mockCarFind.mockImplementationOnce(() => Promise.resolve(newCar));
   const mockUserFindById = jest.spyOn(Users, "findById");
   mockUserFindById.mockImplementationOnce(() => Promise.resolve(1));
   mock.mockImplementationOnce(() => Promise.reject());
   const res = await request(server).put("/6").send(newCar);
   expect(res.status).toBe(500);
   mock.mockRestore();
+  mockCarFind.mockRestore();
 });
 
 test("/cars/:carId DELETE request - deletes a car by carId successfuly- status code 200", async () => {
   const mock = jest.spyOn(Cars, "remove");
+  const mockCarFind = jest.spyOn(Cars, "findBy");
+  mockCarFind.mockImplementationOnce(() => Promise.resolve(newCar));
   mock.mockImplementationOnce(() => Promise.resolve(6));
   const res = await request(server).delete("/6");
   expect(res.status).toBe(200);
-
   expect(res.body).toMatchObject({ message: res.body.message });
   mock.mockRestore();
+  mockCarFind.mockRestore();
 });
 
 test("/cars/:carId DELETE request - returns that car does not exist error - status code 400", async () => {
@@ -160,8 +171,11 @@ test("/cars/:carId DELETE request - returns that car does not exist error - stat
 
 test("/cars/:carId DELETE request - returns error- status code 500", async () => {
   const mock = jest.spyOn(Cars, "remove");
+  const mockCarFind = jest.spyOn(Cars, "findBy");
+  mockCarFind.mockImplementationOnce(() => Promise.resolve(newCar));
   mock.mockImplementationOnce(() => Promise.reject());
   const res = await request(server).delete("/6");
   expect(res.status).toBe(500);
   mock.mockRestore();
+  mockCarFind.mockRestore();
 });
