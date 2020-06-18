949c7152f7f794d97317176eb4ce7d995de65583
Jeffrey <jeffreymartespolanco@hotmail.com>
2020-06-17 21:16:46 -0400

    Email service fully functional, missing html styling

--- nodemailer/email-router.js
+++ nodemailer/email-router.js
@@ -4,22 +4,31 @@ const Users = require("../users/users-model");
 router.post("/send-email", checkId, (req, res) => {
   const { clientId } = req.body;
 <<<<<<< HEAD
+<<<<<<< HEAD
   const subject = "Expresswash Notification";
   const text = "Your job has been completed!";
 =======
   const subject = "Finally it is working";
   const text = "Here is your email that I finally got working";
 >>>>>>> email service implemented but sending email twice atm
+=======
+  const subject = "Expresswash Notification";
+  const text = "Your job has been completed!";
+>>>>>>> Email service fully functional, missing html styling
   Users.findById(clientId).then((user) => {
     sendEmail(user.email, subject, text, (err, data) => {
       if (err) {
         res.status(500).json({ message: "internal error", err });
       } else {
 <<<<<<< HEAD
+<<<<<<< HEAD
         res.status(200).json({ message: "Succesful!" });
 =======
         res.status(200).json({ message: data });
 >>>>>>> email service implemented but sending email twice atm
+=======
+        res.status(200).json({ message: "Succesful!" });
+>>>>>>> Email service fully functional, missing html styling
       }
     });
   });
@@ -33,9 +42,13 @@ function checkId(req, res, next) {
       if (user) {
         req.user = user;
 <<<<<<< HEAD
+<<<<<<< HEAD
         delete req.user.password;
 =======
 >>>>>>> email service implemented but sending email twice atm
+=======
+        delete req.user.password;
+>>>>>>> Email service fully functional, missing html styling
         next();
       } else {
         res.status(404).json({
